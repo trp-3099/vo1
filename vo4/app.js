c24c42f6730972885922e76321cf9374c3096fea
trp-3099 <thisajapeiris@gmail.com>
2026-06-28 10:00:20 +0530

    modified:   vo3/index1.html 	modified:   vo4/app.js

--- vo4/app.js
+++ vo4/app.js
@@ -3,8 +3,24 @@ console.log("View Functions");
 
 let name = "John ";
 let age = 20;
+let ischild = false;
+let customer = {};
+let customer2 = [];
+let salary = 100000;
 
 console.log(name);
 console.log(age);
+console.log(salary);
+console.log(customer);
+console.log(customer2);
+
+
+console.log(ischild);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof ischild);
+console.log(typeof customer);
+console.log(typeof customer2);
+console.log(typeof salary);
 
 alert("Hello Java Script");
