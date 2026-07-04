a307f9822e169cbc089b64c2d936dcd9d4d78675
trp-3099 <thisajapeiris@gmail.com>
2026-07-04 09:44:38 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("Hii");
+
+class customer {
+    name;
+    age;
+    address;
+
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    let customer1 = new customer("John Doe", 30, "123 Main St");
+console.log(customer1);
+
+let customer2 = {
+    name: "Jane Smith",
+    age: 25,
+    address: "456 Elm St"
+
+}
+console.log(customer2);
