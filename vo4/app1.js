3506e3608d5a538bb8c3d048db54ce2242b0c67e
trp-3099 <thisajapeiris@gmail.com>
2026-07-04 10:23:31 +0530

    new file:   vo4/app1.js 	new file:   vo4/index1.html

---
+++ vo4/app1.js
@@ -0,0 +1,171 @@
+// let name = "Saman";
+
+// let age = 18;
+
+// name = "ranil";
+
+// console.log(name);
+// console.log(age);
+
+// alert("Hello icet");
+
+
+// String name = "10";
+
+// ---------------------------------
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
+
+// console.log(typeof name);
+
+// --------------------------------------------
+//  arithmetic operators
+
+// + | - | * | /
+
+// let x = 10;
+// let y = 20;
+
+// let z = x / y;
+
+// console.log(z);
+
+// -------------------------------------------
+
+//  comparison operators
+
+// > | < | >= | <= | == | !=
+// >== | <== | === | !==
+
+// "10"==10 true
+// "11"==10 false
+// "10"===10 false
+// 10===10 true
+
+
+// let x = 11;
+
+// let y = "11";
+
+// console.log(x===y);
+
+
+// -------------------------------------------
+
+//  logical operators
+
+//  && | || | !
+
+// if (!false) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// -------------------------------------------
+
+// if else statement
+
+// let x = 10;
+// let y = 20;
+
+// if (x > y) {
+//     console.log("x is greater than y");
+// } else if (x < y) {
+//     console.log("x is less than y");
+// } else {
+//     console.log("x is equal to y");
+// }
+
+// -------------------------------------------
+
+// switch statement
+
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// -------------------------------------------
+
+// for loop
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// -------------------------------------------
+
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// -------------------------------------------
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+
+// -------------------------------------------
+
+// function
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+// let result = add(10, 20);
+// console.log(result);
+
+
+// public static String add(int x, int y) {
+//     return x + y;
+// }
+
+// -------------------------------------------
+
+// array
+
+// let numbers = [1, true, 3, "saman", 5 , "ranil","sunil"];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// -------------------------------------------
+
+// object
+
+let customer = {
+    name: "Saman",
+    age: 18,
+    isAfterAL: false,
+    salary: 7500.0
+}
+
+console.log(customer);
